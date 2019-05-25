7dc6cb3869f9f7ce004ee093b02a16dc2c6cf7d5
qqzhao <qqzhao2010@gmail.com>
2019-05-25 14:10:40 +0800

    update project

---
+++ src/config/pages_config.js
@@ -0,0 +1,45 @@
+const frameworkPages = {
+  // 'Timer 定时器': 'pages/framework/timer',
+  // 'Timer 定时器2': 'pages/framework/timer',
+
+  'weui: list': '/example/button',
+  'weui: button': '/example/button',
+  'weui: flex': '/example/flex',
+  'weui: grid': '/example/grid',
+  'weui: actionsheet': '/example/actionsheet'
+}
+
+const apiPages = {
+  'weui: list': '/example/button',
+  'weui: button': '/example/button'
+}
+
+const componentPages = {
+  'weui: list': '/example/button'
+}
+
+const appPages = [
+  'pages/framework/index',
+
+  'pages/framework/index',
+
+  'pages/api/index',
+
+  'pages/comp/index',
+
+  'pages/home',
+  'pages/index',
+
+  'example/grid',
+  'example/list',
+  'example/actionsheet',
+  'example/button',
+  'example/flex'
+]
+
+export default {
+  frameworkPages,
+  componentPages,
+  apiPages,
+  appPages
+}
